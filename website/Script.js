0402d5707611da6f8295a2f1396e0f2367c6f5cc
vanellope-glitch <maomaoff1551@gmail.com>
2026-07-27 21:37:39 -0300

    Delete website/Script.js

--- website/Script.js
+++
@@ -1,9 +0,0 @@
-const inviteButton = document.getElementById("invite-button");
-
-inviteButton.addEventListener("click", function (event) {
-    event.preventDefault();
-
-    alert(
-        "O link de convite do Califórnia ainda não foi configurado."
-    );
-});
