ecdf97ae4e4731677d95063746be2442b07df239
vanellope-glitch <maomaoff1551@gmail.com>
2026-07-27 21:37:01 -0300

    Add invite button click event handler

---
+++ website/Script.js
@@ -0,0 +1,9 @@
+const inviteButton = document.getElementById("invite-button");
+
+inviteButton.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    alert(
+        "O link de convite do Califórnia ainda não foi configurado."
+    );
+});
